16f760749b75df6fefeae8b1c2992ceacc240c8c
Minh Huy Tran <huy@perun.network>
2026-08-17 12:43:58 +0200

    feat(contract): add repository and seed data

---
+++ src/main/java/com/example/contractmanagement/ContractRepository.java
@@ -0,0 +1,9 @@
+package com.example.contractmanagement;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractRepository extends JpaRepository<Contract, Long> {
+
+
+}
--- src/main/java/com/example/contractmanagement/ContractmanagementApplication.java
+++ src/main/java/com/example/contractmanagement/ContractmanagementApplication.java
@@ -1,7 +1,12 @@
 package com.example.contractmanagement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class ContractmanagementApplication {
@@ -10,4 +15,12 @@ public class ContractmanagementApplication {
 		SpringApplication.run(ContractmanagementApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(ContractRepository repository) {
+		return (args) -> {
+			// save a few contracts
+			repository.save(new Contract("Jack Bauer", BigDecimal.ONE, LocalDate.now(), 12));
+			repository.save(new Contract("Kim Palmer", BigDecimal.valueOf(12), LocalDate.now(), 10));
+		};
+	}
 }
